f8bcc5e260211f6fa0bb8cb254c44049c4ed99d9
ifat neumann <ineumann19@gmail.com>
2017-12-01 13:52:46 +0200

    writing stage ended, now fixes

--- udp_server.py
+++ udp_server.py
@@ -3,10 +3,39 @@ import sys
 import time
 #local
 def findInMapping(url):
-    if key in mappingDict:
+    if url in mappingDict:
         return True
     else:
         return False
+
+def findAnswer(data,sender_info,s):
+    dataCopy = data.split(',')
+    print dataCopy[0]
+    # search www.bob.com -> bob.com -> com
+    key = data.split(',')[0][1:]
+    while findInMapping(key) == False and key.find('.') != -1:  # search NS
+        key = key[key.find('.') + 1:]
+    # search in other servers
+    # if found = send message
+    if findInMapping(key) == True:
+        msg = mappingDict[key][2]
+        s.sendto(msg, sender_info)  # iterate or recursive
+    # if not found or got to the end of the url - try root
+    else:
+        s.sendto(data, ('127.0.0.1', int(mappingDict['root'])))
+    newData, newSender_info = s.recvfrom(2048)
+    print "Message: ", newData, " from: ", newSender_info, time.clock()
+    # save in cache
+    newData = newData.split(',')
+    mappingDict[newData[0]] = newData
+    # if it's tha answer - return to the client
+    if newData[0] == dataCopy[0]:
+        return
+    else:
+        findAnswer(data, sender_info, s)
+
+
+                #   s.sendto(msg, (source_ip, source_port))
 
 # initialize resolver
 resolver = False
@@ -25,39 +54,12 @@ for line in mappingFile:
     mappingDict[l[0]] = l
 # add root to the dictionary
 mappingDict['root'] = str(sys.argv[1])
-client = ""
 s.bind((source_ip, source_port))
 while True:
     data, sender_info = s.recvfrom(2048)
     print "Message: ", data, " from: ", sender_info, time.clock()
+    findAnswer(data,sender_info,s)
     dataCopy = data.split(',')
-    print dataCopy[0]
-    # if size = 2: quest, if size = 3: answer
-    if len(data)==2:
-        client = sender_info
-    if len(data) == 4:
-        # save in cache
-        mappingDict[dataCopy[0]] = dataCopy
-        #if it's tha answer - return to the client
-    # search www.bob.com
-    key = data.split(',')[0][1:]
-    if findInMapping(key) == True:
-        msg = mappingDict[key][2]
-        s.sendto(msg, sender_info)
-    else:
-        #search bob.com -> com
-        key = key[key.find('.') + 1:]
-        while findInMapping(key) == False and key.find('.')!=-1: #search NS
-            key = key[key.find('.')+1:]
-        # search in other servers
-        # if found = send message
-        if findInMapping(key) == True:
-            msg = mappingDict[key][2]
-            s.sendto(msg, sender_info) #iterate or recursive
-        #if not found - try root
-        else:
-            s.sendto(data, ('127.0.0.1',int(mappingDict['root'])))
-    #if resolver:
-    #   s.sendto(msg, (source_ip, source_port))
+    s.sendto(mappingDict[dataCopy[0]], sender_info)
 
 
