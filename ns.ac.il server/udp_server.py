9c6ea6b0f10c02d8eb69ed873c9dcb704913f387
ifat neumann <ineumann19@gmail.com>
2017-12-03 20:28:28 +0200

    fixed client

--- ns.ac.il server/udp_server.py
+++ ns.ac.il server/udp_server.py
@@ -3,67 +3,71 @@ import sys
 import time
 import ast
 #local
+#arguments: [my IP] [my port] [mapping file name] [my root server] [resolver or not]
 
 # check if the ttl passed, and if so - delete from cache.
 # return true if the key is ok, and false otherwise
 def timeStampCheck(key):
-    if findInMapping(key)==False:
+    if findInCache(key)==False:
         return False
-    timeStamp = mappingDict[key][4]
-    ttl = mappingDict[key][3]
+    timeStamp = cache[key][4]
+    ttl = cache[key][3]
     if timeStamp != '-1' and (time.clock() - float(timeStamp) > float(ttl) ):
-        del mappingDict[key]
+        del cache[key]
         return False
     return  True
 
-def findInMapping(url):
-    if url in mappingDict:
+# check if the url is in the cache
+def findInCache(url):
+    if url in cache:
         return True
     else:
         return False
 
 def findAnswerNotResolver(data,sender_info,s):
     dataCopy = data[1:-1].split(',')
+
     # search www.bob.com -> bob.com -> com
     key = data.split(',')[0][1:]
-    while findInMapping(key) == False and key.find('.') != -1:  # search NS
+    while findInCache(key) == False and key.find('.') != -1:  # search NS
         key = key[key.find('.') + 1:]
-    # if found = send details
-    if findInMapping(key) == True:
+
+    # if found - send details
+    if findInCache(key) == True:
         if key == dataCopy[0]:
-            msg = str(mappingDict[key])
+            msg = str(cache[key])
         else:
             # found a helpful NS
-            nsUrl = mappingDict[key][2]
+            nsUrl = cache[key][2]
             # send the NS and it's Ip
-            msg = str(mappingDict[key]) + "\n" + str(mappingDict[nsUrl])
+            msg = str(cache[key]) + "\n" + str(cache[nsUrl])
+
     # if not found or got to the end of the url
     else:
         msg = "not found"
     s.sendto(msg, sender_info)
 
 def findAnswerResolver(data,sender_info,s,source_ip,source_port):
-    dataCopy = data[-1:1].split(',')
     # search www.bob.com -> bob.com -> com
     key = data.split(',')[0][1:]
     # if found the url - return
-    if findInMapping(key) == True and timeStampCheck(key):
+    if findInCache(key) == True and timeStampCheck(key):
         return True
-    print "key: "+key+"findInMapping(key): "+str(findInMapping(key))+"timeStampCheck(key): "+str(timeStampCheck(key))
+
     # while the key is not in the cache or it is but the ttl passed:
-    while (findInMapping(key) == False or (findInMapping(key) and timeStampCheck(key)==False)) and key.find('.') != -1:  # search NS
+    while (findInCache(key) == False or (findInCache(key) and timeStampCheck(key)==False)) and key.find('.') != -1:
+        # search NS
         key = key[key.find('.') + 1:]
-        print "inside while: "+key
 # ask other servers
+
     # if found = send message to him
-    if findInMapping(key) and timeStampCheck(key):
-        nsUrl = mappingDict[key][2]
-        server = mappingDict[nsUrl]
-        print str(server)
+    if findInCache(key) and timeStampCheck(key):
+        nsUrl = cache[key][2]
+        server = cache[nsUrl]
         s.sendto(data, ('127.0.0.1',int(server[2])))
     # if not found or got to the end of the url - send message to root
     else:
-        s.sendto(data, ('127.0.0.1', int(mappingDict['root'])))
+        s.sendto(data, ('127.0.0.1', int(cache['root'])))
     return recursive(data,s)
 
 def recursive(data,s):
@@ -71,62 +75,65 @@ def recursive(data,s):
     #get answer
     newData, newSender_info = s.recvfrom(2048)
     print "Message: ", newData, " from: ", newSender_info, time.clock()
-    first=""
     if newData == "not found":
         return False
+    # if we got a GluedRR massage
     if newData.find('\n')!=-1:
         first = newData[:newData.find('\n')]
         first = ast.literal_eval(first)
         first.append(time.clock())
-        #add to dict
+
+        #add to cache the first one
         newKey = newData[1:-1].split(',')[0][1:-1]
-        mappingDict[newKey] = first
+        cache[newKey] = first
         newData = newData[newData.find('\n')+1:]
+
     newKey = newData[1:-1].split(',')[0][1:-1]
     #if = key - return
     if newKey == dataCopy[0]:
         newData = ast.literal_eval(newData)
         newData[4] = time.clock()
-        mappingDict[newKey] = newData
+        cache[newKey] = newData
         return True
-    #else - GOT NEW DESTINATION
+    #else - GOT NEW DESTINATION to ask
+
     #save in cache
     newData = ast.literal_eval(newData)
     newData.append(time.clock())
-    mappingDict[newData[0]] = newData
+    cache[newData[0]] = newData
     # send him the data (newData = new dest)
     s.sendto(data, ('127.0.0.1', int(newData[2])))
     return recursive(data,s)
 
 # initialize resolver
 resolver = False
-if str(sys.argv[2]) == "resolver":
+if str(sys.argv[5]) == "resolver":
     resolver = True
 
 s = socket(AF_INET, SOCK_DGRAM)
-source_ip = '0.0.0.0'
-source_port = 12445
+source_ip = sys.argv[1]
+source_port = sys.argv[2]
 
 # save mapping's data in a dictionary
-mappingFile=open("mapping.txt","r")
-mappingDict = {}
+mappingFile=open(sys.argv[3],"r")
+cache = {}
 for line in mappingFile:
     l = line.split()
     l.append('-1')
-    mappingDict[l[0]] = l
-# add root to the dictionary
-mappingDict['root'] = str(sys.argv[1])
-s.bind((source_ip, source_port))
+    cache[l[0]] = l
+
+# add root to the cache
+cache['root'] = str(sys.argv[4])
+s.bind((source_ip, int(source_port)))
+
 while True:
     data, sender_info = s.recvfrom(2048)
     print "Message: ", data, " from: ", sender_info, time.clock()
     if resolver:
         if findAnswerResolver(data,sender_info,s,source_ip,source_port):
             dataCopy = data[1:-1].split(',')
-            s.sendto(str(mappingDict[dataCopy[0]]), sender_info)
+            s.sendto(str(cache[dataCopy[0]]), sender_info)
         else:
             s.sendto("not found", sender_info)
     else:
         findAnswerNotResolver(data, sender_info, s)
-
-
--- udp_client.py
+++ udp_client.py
@@ -1,15 +1,42 @@
 from socket import socket, AF_INET, SOCK_DGRAM
 import sys
+import time
+import ast
+
+def findInCache(key):
+    if not (key in cache):
+        return False
+    if len(cache[key])<2:
+        return False
+    timeStamp = cache[key][4]
+    ttl = cache[key][3]
+    if timeStamp != '-1' and (time.clock() - float(timeStamp) > float(ttl) ):
+        del cache[key]
+        return False
+    return  True
 
 s = socket(AF_INET, SOCK_DGRAM)
 #server's info
-dest_ip = '127.0.0.1'
-dest_port = int(str(sys.argv[1]))
+dest_ip = sys.argv[1]
+dest_port = int(sys.argv[2])
+
+# create cache for the client
+cache = {}
 
 msg = raw_input("Message to send: ")
 while not msg == 'quit':
-    s.sendto(msg, (dest_ip,dest_port))
-    data, _ = s.recvfrom(2048)
-    print "Server sent: ", data
+    key = msg.split(',')[0][1:]
+    if findInCache(key):
+        print "You have in cache: "+str(cache[key])
+    else:
+        s.sendto(msg, (dest_ip,dest_port))
+        data, _ = s.recvfrom(2048)
+        print "Server sent: ", data
+
+        # add to cache
+        key = data[1:-1].split(',')[0][1:-1]
+        data = ast.literal_eval(data)
+        data[4] = time.clock()
+        cache[key] = data
     msg = raw_input("Message to send: ")
 s.close()
